002528a15516bb97f37a3df227a11aae3dc27b7d
czyzu05 <damian.czyz1998@gmail.com>
2020-10-23 20:44:33 +0200

    Updates

---
+++ precache-manifest.13d174f5a17090ec35249c92af41659b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dca249ec14218968e3295c399ae6cea",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "21e9a934540eb2aac648",
+    "url": "/todo-app/static/js/2.6b41115f.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/todo-app/static/js/2.6b41115f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b104daac799dad2ba5",
+    "url": "/todo-app/static/js/main.2c0b9cd8.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
